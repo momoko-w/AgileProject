1a0ef6dd246f5de76876f20799e3cbe004e352a0
momoko-w <23170782+momoko-w@users.noreply.github.com>
2019-01-22 12:37:41 +0000

    Added exam classfunctionality + including tests

--- test/agileproject/AgileProjectTest.java
+++ test/agileproject/AgileProjectTest.java
@@ -45,8 +45,6 @@ public class AgileProjectTest {
         System.out.println("main");
         String[] args = null;
         AgileProject.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
